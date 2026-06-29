174cf2dbc54011661092352ce4bcd42434b587c4
Excash-earn <businessinfoblog1@gmail.com>
2026-06-29 17:29:06 +0100

    Add Firebase configuration and initialization code
    Added Firebase configuration and initialization.

---
+++ css/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCslwkUXF4knMUoQwu3FJsR416_m2TJMAo",
+  authDomain: "excash-6b20c.firebaseapp.com",
+  projectId: "excash-6b20c",
+  storageBucket: "excash-6b20c.firebasestorage.app",
+  messagingSenderId: "813981515504",
+  appId: "1:813981515504:web:a00015d11754c8d6148c35",
+  measurementId: "G-RN318N2NGQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
